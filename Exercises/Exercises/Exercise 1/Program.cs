17a6379ed318e1ac08fab6fea3134c2cd6d69e29
Sampsa Tervo <AB6912@student.jamk.fi>
2023-09-11 18:18:50 +0300

    Ex1

---
+++ Exercises/Exercises/Exercise 1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) { 
+                Console.WriteLine("Give points: ");
+                string points = Console.ReadLine();
+                try
+                {
+                    int pointsInt = Int32.Parse(points);
+                    if(pointsInt > 0 && pointsInt <= 19)
+                    {
+                        Console.WriteLine("Your grade is 0.");
+                    }
+                    else if(pointsInt >= 20 && pointsInt <= 29)
+                    {
+                        Console.WriteLine("Your grade is 1.");
+                    }
+                    else if (pointsInt >= 30 && pointsInt <= 39)
+                    {
+                        Console.WriteLine("Your grade is 2.");
+                    }
+                    else if (pointsInt >= 40 && pointsInt <= 49)
+                    {
+                        Console.WriteLine("Your grade is 3.");
+                    }
+                    else if (pointsInt >= 50 && pointsInt <= 59)
+                    {
+                        Console.WriteLine("Your grade is 4.");
+                    }
+                    else if (pointsInt >= 60 && pointsInt <= 70)
+                    {
+                        Console.WriteLine("Your grade is 5.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Points need to be an integer between 0 and 70!");
+                    }
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                catch
+                {
+                    Console.WriteLine($"Couldn't convert {points} into a string!");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+        }
+    }
+}
